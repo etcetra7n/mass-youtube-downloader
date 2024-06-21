49606ee729e3f80ad7fdcd6701dc8d90aa1b706c
John Anchery <john.anchery@mail.ru>
2024-06-21 12:14:46 +0530

    Fix

---
+++ ytd.py
@@ -0,0 +1,10 @@
+from pytube import YouTube
+
+with open("links.txt", "r") as f:
+    links = f.readlines()
+    print("Downloading " + str(len(links)) + " videos...")
+    count = 0
+    for link in links:
+        yt = YouTube(link).streams.first().download("./downloads")
+        count += 1
+        print(f"Downloaded ({count}/{len(links)})")
